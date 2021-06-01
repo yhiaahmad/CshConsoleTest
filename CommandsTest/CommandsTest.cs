c24bb84427ffc8921e18d0f19aae388ed6bfadee
farouk shtewee <farok95@hotmail.com>
2021-06-01 16:14:18 +0300

    ConsoleTest

--- CommandsTest/CommandsTest.cs
+++ CommandsTest/CommandsTest.cs
@@ -12,6 +12,19 @@ namespace CshConsoleTest
 	{
 		public const string CMD_ECHO_PARAM= "hello echo";
 		public const string CMD_LINE_ECHO= "echo(hello world)";
+		/******************************************************/
+		public const string CMD_ADD_PARAM = "5,3";
+		public const string CMD_LINE_ADD = "ADD(5,3)";
+		/******************************************************/
+		public const string CMD_SUB_PARAM = "5,3";
+		public const string CMD_LINE_SUB = "SUB(5,3)";
+		/******************************************************/
+		public const string CMD_MULT_PARAM = "5,3";
+		public const string CMD_LINE_MULT = "MULT(5,3)";
+		/******************************************************/
+		public const string CMD_DIV_PARAM = "5,3,2";
+		public const string CMD_LINE_DIV = "DIV(5,3,2)";
+		/******************************************************/
 
 		private static bool CommandEcho(string[] parameters)
 		{
@@ -43,6 +56,38 @@ namespace CshConsoleTest
 			Assert.IsTrue(AppCommands.CommandEcho(parameters));
 			return;
 		}
+		//	Test the add command function.
+		[TestMethod]
+		public void TestADD()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_ADD);
+			Assert.IsTrue(AppCommands.CommandADD(parameters));
+			return;
+		}
+		//	Test the sub command function.
+		[TestMethod]
+		public void TestSUB()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_SUB);
+			Assert.IsTrue(AppCommands.CommandSUB(parameters));
+			return;
+		}
+		//	Test the mult command function.
+		[TestMethod]
+		public void TestMULT()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_MULT);
+			Assert.IsTrue(AppCommands.CommandMULT(parameters));
+			return;
+		}
+		//	Test the div command function.
+		[TestMethod]
+		public void TestDIV()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_DIV);
+			Assert.IsTrue(AppCommands.CommandDIV(parameters));
+			return;
+		}
 
 		//	Test the commands list using the echo command function.
 		[TestMethod]
@@ -57,6 +102,26 @@ namespace CshConsoleTest
 
 			//	Execute the echo command in the command list.
 			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_ECHO));
+			//	Check adding ADD command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ADD, AppCommands.CommandADD));
+
+			//	Execute the ADD command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_ADD));
+			//	Check adding SUB command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_SUB, AppCommands.CommandSUB));
+
+			//	Execute the SUB command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_SUB));
+			//	Check adding MULT command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_MULT, AppCommands.CommandMULT));
+
+			//	Execute the MULT command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_MULT));
+			//	Check adding DIV command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_DIV, AppCommands.CommandDIV));
+
+			//	Execute the DIV command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_DIV));
 			return;
 		}
 	};
